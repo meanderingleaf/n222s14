3d29580d8eba39f4396c4e0088f68061a5c630e0
Travis <travis.faas@gmail.com>
2014-03-04 12:11:29 -0500

    More Phaser stuff

---
+++ examples/phaserSprites/sprites/sprites.js
@@ -0,0 +1,25 @@
+{
+	"frames": {
+		"slice01_01.png": {
+			"frame": {"x":0, "y":0, "w":23, "h":31},
+			"spriteSourceSize": {"x":0,"y":0,"w":23,"h":31},
+			"sourceSize": {"w":23,"h":31}
+		},
+		"slice02_02.png": {
+			"frame": {"x":24, "y":0, "w":23, "h":29},
+			"spriteSourceSize": {"x":0,"y":0,"w":23,"h":29},
+			"sourceSize": {"w":23,"h":29}
+		},
+		"slice03_03 - Copy.png": {
+			"frame": {"x":24, "y":30, "w":21, "h":24},
+			"spriteSourceSize": {"x":0,"y":0,"w":21,"h":24},
+			"sourceSize": {"w":21,"h":24}
+		}
+
+	},
+	"meta": {
+		"image": "sprites.png",
+		"size": {"w": 48, "h": 55},
+		"scale": "1"
+	}
+}
